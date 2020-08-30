b6a635104fd7597691148093301bd32a8d1684de
Yaseen Saif AlShaibani <yasenshibane77@gmail.com>
2020-08-31 00:16:06 +0300

    init commit

---
+++ src/main.kt
@@ -0,0 +1,5 @@
+fun main() {
+    var myTeam = arrayOf("yaseen","ahmed","omar","ziad");
+    println("the first element of myTeam array : ${myTeam.first()}")
+    println("the last element of myTeam array : ${myTeam.last()}")
+}
